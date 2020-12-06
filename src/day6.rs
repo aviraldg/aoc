8debcbd6877706fc31264c99c9df25dcec4ac302
Aviral Dasgupta <me@aviraldg.com>
2020-12-06 18:37:10 +0000

    2020: Day 6 solution

---
+++ src/day6.rs
@@ -0,0 +1,17 @@
+use std::collections::{HashMap, HashSet};
+
+#[aoc_generator(day6)]
+fn input_generator(input: &str) -> Vec<HashSet<char>> {
+  input.split("\n\n").map(|group| {
+    group.lines().map(|person| {
+      person.chars().collect::<HashSet<char>>()
+    }).fold(('a'..='z').collect::<HashSet<char>>(), |i, e| {
+      i.intersection(&e).copied().collect::<HashSet<char>>()
+    })
+  }).collect()
+}
+
+#[aoc(day6, part2)]
+fn solve_part2(input: &Vec<HashSet<char>>) -> usize {
+  input.iter().map(|set| set.len()).sum()
+}
